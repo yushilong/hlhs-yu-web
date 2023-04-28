bd0e5410d3186ebfc1b517aec61d522ff26dd350
yushilong <yushilong7443@yundasys.com>
2023-04-28 14:29:04 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,17 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 import admin from "./admin";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue')
-        },
-        ...admin
-    ]
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    ...admin,
+  ],
+});
 
-export default router
+export default router;
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode}) => {
+    // 获取当前环境的配置
+    const config = loadEnv(mode, './')
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        //开发配置  npm run dev
+        server: {
+            port: 4001, //端口号
+            strictPort: true, //是否是严格的端口号，如果true，端口号被占用的情况下，vite会退出
+            host: 'localhost',
+            cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
+            https: false, //是否支持http2 如果配置成true 会打开https://localhost:3001/xxx;
+            // open: true, //是否自动打开浏览器
+            // 反向代理 跨域配置
+            proxy: {
+                '/api': {
+                    target: config.VITE_APP_API_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
+            }
+        }
     }
-  },
-  //开发配置  npm run dev
-  server: {
-    port: 4001, //端口号
-    strictPort: true, //是否是严格的端口号，如果true，端口号被占用的情况下，vite会退出
-    host: 'localhost',
-    cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
-    https: false, //是否支持http2 如果配置成true 会打开https://localhost:3001/xxx;
-    // open: true, //是否自动打开浏览器
-    // 反向代理 跨域配置
-    proxy: {
-      '/api': {
-        target: "http://localhost:5001/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
